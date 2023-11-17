bc33c0e7c5621d9b0bd29c618fa9ccb3c5cb0beb
gustavoduarte-dcx <gustavo.duarte@datacentrics.com.br>
2023-11-17 14:22:32 -0300

    Refinando módulo v1

--- notification.tf
+++ notification.tf
@@ -4,7 +4,7 @@ resource "oci_ons_notification_topic" "test_notification_topic" {
     for_each = var.notification_topic_name
     #Required
     compartment_id = var.compartment_id
-    name = each.key
+    name = each.key.value
 
     #Optional
     #freeform_tags  = each.value["tags"]
