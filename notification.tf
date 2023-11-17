c3d4ac999e132c679aa09f1b9d7ecc6cafc0eec1
gustavoduarte-dcx <gustavo.duarte@datacentrics.com.br>
2023-11-17 16:56:11 -0300

    Atualizando módulo

--- notification.tf
+++ notification.tf
@@ -4,8 +4,8 @@ resource "oci_ons_notification_topic" "test_notification_topic" {
     for_each = var.notification_topics
     #Required
     compartment_id = var.compartment_id
-    name = each.key
-    description = each.key
+    name = each.value.name
+    description = each.value.description
 
     #Optional
 
