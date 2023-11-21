8b2a9b1510f376c0feb11e4cd6c125294a425803
gustavoduarte-dcx <gustavo.duarte@datacentrics.com.br>
2023-11-21 17:47:15 -0300

    Adicionando uma output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "notification_id" {
     value = [for notification in oci_ons_notification_topic.test_notification_topic : notification.id]
 }
+
+
+output "notification_name" {
+    value = [for notification in oci_ons_notification_topic.test_notification_topic : notification.name]
+}
